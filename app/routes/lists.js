0bc95c51df0918a888b4ee333cafeb169760ec49
luzcry <mcrodriguezt618@gmail.com>
2021-02-04 04:21:09 -0500

    adding models

--- app/routes/lists.js
+++ app/routes/lists.js
@@ -1,3 +1,7 @@
 import Route from '@ember/routing/route';
 
-export default class ListsRoute extends Route {}
+export default class ListsRoute extends Route {
+  model(){
+    return this.store.findAll("list");
+  }
+}
